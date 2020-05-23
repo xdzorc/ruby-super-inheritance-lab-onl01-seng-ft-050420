1f638345edb6cd773d2328c5b508cd276720d7a8
DAZUN XING <dazun.xing@gmail.com>
2020-05-23 07:27:48 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -4,11 +4,11 @@ class ChattyStudent < Student
     puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end
   
-  def raise_hand 
+  def raise 
     10.times {super}
   end
 end
 
 a=ChattyStudent.new 
 a.hello
-a.raise_hand
+a.raise
